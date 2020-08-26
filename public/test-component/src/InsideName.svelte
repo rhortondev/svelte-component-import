e5e6b5e1ba01847421721eb86c39db210dfe61dd
Roger Horton <rhorton@amdpi.com>
2020-08-26 17:08:19 -0400

    Initial project source code load

---
+++ public/test-component/src/InsideName.svelte
@@ -0,0 +1,22 @@
+
+<svelte:options accessors/>
+
+<script>
+    export var name = "Jim";
+</script>
+
+<style>
+    .special {
+        font-style: italic;
+        border: none;
+        border-top: 2px solid #ccc;
+        border-bottom: 2px solid #ccc;
+        border-radius: 3px 3px 0 0;
+    }
+</style>
+
+<div class="special">
+    <h1>
+        Dammit, {name}, I'm a doctor not a bricklayer
+    </h1>
+</div>
---
+++ public/test-component/src/main.js
@@ -0,0 +1,5 @@
+import ImportName from './InsideName.svelte';
+
+window.OutsideName = function (options) {
+    return new ImportName(options);
+};
